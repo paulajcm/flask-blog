6cb9c580f7a396c27798cc31bf02c74032d48bbd
Paula Castilho de Mendonca <paula.mendonca@eldorado.org.br>
2018-07-17 07:22:19 -0400

    Add registration page

---
+++ author/form.py
@@ -0,0 +1,20 @@
+from flask_wtf import FlaskForm
+from wtforms import validators, StringField, PasswordField
+from wtforms.fields.html5 import EmailField
+
+
+class RegisterForm(FlaskForm):
+    fullname = StringField('Full name', [validators.Required()])
+    email = EmailField('Email', [validators.Required()])
+    username = StringField('Username', [
+        validators.DataRequired(),
+        validators.Length(min=4, max=25)
+    ])
+    password = PasswordField('New Password', [
+        validators.DataRequired(),
+        validators.EqualTo('confirm', message='Passwords must match'),
+        validators.Length(min=4, max=80)
+    ])
+    confirm = PasswordField('Repeat password')
+
+    #TODO Check why validators are not validating
--- author/views.py
+++ author/views.py
@@ -1,6 +1,21 @@
 from flask_blog import app
+from flask import render_template, redirect, url_for
+from flask_blog.author.form import RegisterForm
 
 
 @app.route('/login')
 def login():
     return "welcome"
+
+
+@app.route('/register', methods=('GET', 'POST'))
+def register():
+    form = RegisterForm()
+    if form.validate_on_submit():
+        return redirect(url_for('success'))
+    return render_template('author/register.html', form=form)
+
+
+@app.route('/success')
+def success():
+    return 'Success'
