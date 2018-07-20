d83cec3b595494906f99587918085dc904e77902
Paula Castilho de Mendonca <paula.mendonca@eldorado.org.br>
2018-07-20 07:02:45 -0400

    Using Migrate to handle database changes

--- __init__.py
+++ __init__.py
@@ -1,9 +1,13 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
 
 app = Flask(__name__)
 app.config.from_object('settings')
 db = SQLAlchemy(app)
+
+# migrations
+migrate = Migrate(app, db)
 
 from flask_blog.blog import views
 from flask_blog.author import views
--- manage.py
+++ manage.py
@@ -3,9 +3,11 @@ sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 
 from flask_script import Manager, Server
 from flask_blog import app
+from flask_migrate import MigrateCommand
 
 
 manager = Manager(app)
+manager.add_command('db', MigrateCommand)
 
 manager.add_command("runserver", Server(
     use_debugger=True,
