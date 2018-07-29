215bf16e595be4234357e30f479490faec9edd74
Paula Castilho de Mendonca <paula.mendonca@eldorado.org.br>
2018-07-29 12:39:34 -0400

    Setup unit test

---
+++ tests.py
@@ -0,0 +1,35 @@
+# Set the path
+import os, sys
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+
+import unittest
+import sqlalchemy
+from flask_sqlalchemy import SQLAlchemy
+
+from flask_blog import app, db
+
+# models
+from flask_blog.author.models import *
+from flask_blog.blog.models import *
+
+class UserTest(unittest.TestCase):
+    def setUp(self):
+        db_username = app.config['DB_USERNAME']
+        db_password = app.config['DB_PASSWORD']
+        db_host = app.config['DB_HOST']
+        self.db_uri = "mysql+pymysql://%s:%s@%s/%s" % (db_username, db_password, db_host)
+        app.config['TESTING'] = True
+        app.config['WTF_CSRF_ENABLED'] = False
+        app.config['BLOG_DATABSE_NAME'] = 'test_blog'
+        app.config['SQLALCHEMY_DATABSE_URI'] = self.db_uri + app.config['BLOG_DATABASE_NAME']
+        engine = sqlalchemy.create_engine(self.db_uri)
+        conn = engine.connect()
+        conn.execute('commit')
+        conn.execute('CREATE DATABASE ' + app.config['BLOG_DATABASE_NAME'])
+
+    def tearDown(self):
+        db.session.remove()
+        engine = sqlalchemy.create_engine(self.db_uri)
+        conn = engine.connect()
+        conn.execute('commit')
+        conn.execute('DROP DATABASE ' + app.config['BLOG_DATABASE_NAME'])
